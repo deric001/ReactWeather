c80c06d64a8860be8d2926f24c600b65f234a2a9
deric bertrand <dericbertrand@derics-MacBook-Pro.local>
2017-01-26 12:09:14 -0600

    Update middleware for localhost

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 var express = require ('express');
-
 //create our app
 var app = express();
 
 // get the port from the haduko environment variable
 const PORT = process.env.port || 3000;
 
-
-app.use(function (req, res, next) {
-  if(req.headers['x-forwarded-proto'] == 'http') {
-    next();
-  } else {
+app.use(function (req, resp, next) {
+  if(req.headers['x-forwarded-proto'] == 'https') {
     resp.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
   }
 });
 
