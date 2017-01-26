6c1f414b650d743660936a480d5377a240aace46
deric bertrand <dericbertrand@derics-MacBook-Pro.local>
2017-01-26 14:28:52 -0600

    Add some styling for Example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var {Link} = require('react-router');
+
 
 // var Examples = React.createClass({
 //   render: function () {
@@ -11,8 +13,16 @@ var React = require('react');
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-        <p>Welcome to our Examples page.</p>
+      <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out:</p>
+        <ol>
+          <li>
+            <Link to='/?location=Naperville'>Naperville, IL</Link>
+          </li>
+          <li>
+            <Link to='/?location=Rio'>Rio, Brazil</Link>
+          </li>
+        </ol>
     </div>
   );
 };
