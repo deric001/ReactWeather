894f00224e9f4bdd7dc6bea609d04b425d3aec5b
deric bertrand <dericbertrand@derics-MacBook-Pro.local>
2017-01-26 11:11:48 -0600

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ var express = require ('express');
 //create our app
 var app = express();
 
+// get the port from the haduko environment variable
+const PORT = process.env.port || 3000;
+
+
+app.use(function (req, res, next) {
+  if(req.headers['x-forwarded-proto'] == 'http') {
+    next();
+  } else {
+    resp.redirect('http://' + req.hostname + req.url);
+  }
+});
+
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-  console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+  console.log('Express server is up on port ' + PORT);
 });
